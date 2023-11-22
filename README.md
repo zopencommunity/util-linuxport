ca438247101c2e377a1f4569bb049e4a5cc85d32
Igor Todorovski <itodorov@ca.ibm.com>
2023-11-22 16:58:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+util-linux
+
+util-linux is a standard package distributed by the Linux Kernel Organization for use as part of the Linux operating system.
---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/util-linuxport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/util-linuxport.git'), string(name: 'PORT_DESCRIPTION', value: 'util-linux is a standard package distributed by the Linux Kernel Organization for use as part of the Linux operating system.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+  }
+}
