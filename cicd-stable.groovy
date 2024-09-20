49b56f528963ac34295e5b8bb005553de33d63fb
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:12 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/util-linuxport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/util-linuxport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/util-linuxport.git'), string(name: 'PORT_DESCRIPTION', value: 'util-linux is a standard package distributed by the Linux Kernel Organization for use as part of the Linux operating system.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/util-linuxport.git'), string(name: 'PORT_DESCRIPTION', value: 'util-linux is a standard package distributed by the Linux Kernel Organization for use as part of the Linux operating system.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
